10ec86146c172d022ef91c25686de27d9bf5b71c
CarsAndCode007 <programmingandcoding2020@gmail.com>
2024-11-04 21:45:59 -0600

    Initial commit

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, inject } from '@angular/core';
+import { type Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
+import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
-export class TaskComponent {}
+export class TaskComponent {
+  @Input({ required: true }) task!: Task;
+  private tasksService = inject(TasksService);
+  onCompleteTask() {
+    this.tasksService.removeTask(this.task.id);
+  }
+}
